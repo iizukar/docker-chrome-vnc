400f4073339433bc247b6a705b9c374723c14a70
iizukar <czechia.deitan@gmail.com>
2025-02-24 23:06:47 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ export DISPLAY=:1
 fluxbox &
 
 # Start x11vnc (VNC server)
-x11vnc -display :1 -forever -shared -nopw &
+x11vnc -display :1 -forever -shared -nopw -rfbport 5900 &
 
-# Start Midori (lightweight browser)
-midori
+# Start Falkon (lightweight browser)
+falkon
