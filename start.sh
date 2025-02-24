e7af1a7f351857bd06a855c70d30b83e9c09e024
iizukar <czechia.deitan@gmail.com>
2025-02-24 23:51:05 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-# Start Xvfb in background
-Xvfb :0 -screen 0 1280x720x16 &
+# Start Xvfb with 24-bit color depth
+Xvfb :0 -screen 0 1280x720x24 +extension GLX +render -nolisten tcp &
 
-# Start fluxbox window manager
-fluxbox &
+# Start fluxbox
+fluxbox -display :0 &
 
-# Start chromium in fullscreen (headless mode)
-chromium --no-sandbox --start-maximized --disable-gpu --remote-debugging-port=9222 &
+# Configure Chromium flags
+chromium --no-sandbox \
+         --disable-gpu \
+         --disable-software-rasterizer \
+         --disable-dev-shm-usage \
+         --use-gl=swiftshader \
+         --ignore-gpu-blocklist \
+         --start-maximized \
+         --remote-debugging-port=9222 &
 
 # Start VNC server
 x11vnc -display :0 -forever -noxdamage -passwd secret -rfbport $VNC_PORT &
@@ -15,5 +22,4 @@ x11vnc -display :0 -forever -noxdamage -passwd secret -rfbport $VNC_PORT &
 # Start noVNC
 websockify -D --web=/usr/share/novnc $NOVNC_PORT localhost:$VNC_PORT
 
-# Keep container running
 tail -f /dev/null
