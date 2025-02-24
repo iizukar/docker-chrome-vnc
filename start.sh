9adad9133429662b57b12c69b51b2ac7bbdcabfb
iizukar <czechia.deitan@gmail.com>
2025-02-24 22:56:38 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,13 @@
-#!/bin/bash
-exec /usr/bin/supervisord -c /etc/supervisord.conf
+#!/bin/sh
+# Start Xvfb (virtual framebuffer)
+Xvfb :1 -screen 0 1024x768x16 &
+export DISPLAY=:1
+
+# Start Fluxbox (lightweight window manager)
+fluxbox &
+
+# Start x11vnc (VNC server)
+x11vnc -display :1 -forever -shared -nopw &
+
+# Start Midori (lightweight browser)
+midori
